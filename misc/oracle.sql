579848de7814fd168ce9aaa7c00a4a8aa2b205c3
trini000 <chen_axiao@hotmail.com>
2013-05-03 04:45:36 -0700

    Notes

--- misc/oracle.sql
+++ misc/oracle.sql
@@ -1,1 +1,19 @@
-DBMS_OUTPUT.PUT_LINE(v_index);
+DECLARE
+productFound NUMBER;
+bFound BOOLEAN;
+
+BEGIN
+select count(*) INTO productFound from ott_vodproductcontent where productid=4001;
+bFound :=(productFound > 0);
+
+IF productFound= 0  THEN
+	DBMS_OUTPUT.PUT_LINE('v_index=');
+ELSE
+	DBMS_OUTPUT.PUT_LINE('1111');
+END IF;
+
+IF (bFound) THEN DBMS_OUTPUT.PUT_LINE('found!');
+ELSE DBMS_OUTPUT.PUT_LINE('Nothing!');
+END IF;
+
+END;
